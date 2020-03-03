a8fc2f530de0a86d16def18a245ceceb9e17c280
jonathankuik <mymac@Jonathan-Kuik-MacBook.local>
2020-03-03 13:33:20 -0600

    added tasks to seed institution and demographic tables

--- paper_chasers/db/seeds.rb
+++ paper_chasers/db/seeds.rb
@@ -5,42 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-school = Institution.find_by_id(2)
-puts school.name
-CostAndFinancialAidProfile.create(institution_id: school.id, percent_awarded_fin_aid: 50, )
-
-
 """
 require 'csv'
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'InstitutionEntityData.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'DemographicEntityData.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-    Institution.create(
-        unitId: row['unitid'],
-        name: row['name'],
-        sector_of_institition: row['sector_of_institution'],
-        institutional_control_or_affiliation: row['institutional_control_or_affiliation'],
-        institutional_category: row['institutional_category'],
-        hbcu: row['hbcu'],
-        state_abbreviation: row['state_abbreviation'],
-        degree_of_urbanization: row['degree_of_urbanization'],
-        carnegie_classification_basic: row['carnegie_classification_basic'],
-        carnegie_classification_undergraduate: row['canegie_classification_undergraduate'],
-        carnegie_classification_graduate: row['carnegie_classification_graduate'],
-        nces_data_reporting_category: row['nces_data_reporting_category'],
-        highest_degree_offered: row['highest_degree_offered'],
-        percent_admitted: row['percent_admitted'],
-        percent_admitted_men: row['percent_admitted_men'],
-        percent_admitted_women: row['percent_admitted_women'],
-        total_enrollment: row['total_enrollment'],
-        open_enrollement: row['open_enrollment'],
-        tes_scores_considered: row['tes_scores_considered'],
-        percent_submitting_act: row['percent_submitting_act'],
-        percent_submitting_sat: row['percent_submitting_sat'],
+    institution = Institution.find_by(unitId: row['UnitId'])
+    institution.StudentDemographicProfile.create(
+        undergrad_total= row['UndergradTotal'],
+        undergrad_men= row['UndergradMen'],
+        undergrad_women= row['UndergradWomen'],
+        american_indian_total_undergrad= row['AmericanIndianTotalUndergrad'],
+        asian_total_undergrad= row['AsianTotalUndergrad'],
+        black_or_african_american_total_undergrad= row['BlackOrAfricanAmericanTotalUndergrad'],
+        hispanic_total_undergrad= row['HispanicTotalUndergrad'],
+        native_hawaiian_pacific_islander_total_undergrad= row['NativeHawaiianPacificIslanderTotalUndergrad'],
+        white_undergrad= row['WhiteUndergrad'],
+        graduate_student_total= row['GraduateStudentTotal'],
+        graduate_student_men= row['GraduateStudentMen'],
+        graduate_student_woman= row['GraduateStudentWomen'],
+        american_indian_total_grad= row['AmericanIndianTotalGrad'],
+        asian_total_grad= row['AsianTotalGrad'],
+        black_or_african_american_total_grad= row['BlackOrAfricanAmericanTotalGrad'],
+        hispanic_total_grad= row['HispanicTotalGrad'],
+        native_hawaiian_pacific_islander_total_grad= row['NativeHawaiianPacificIslanderTotalGrad'],
+        white_grad= row['WhiteGrad'],
+        first_time_undergrad_us= row['FirstTimeUndergradUS'],
+        first_time_undergrad_foreign= row['FirsttimeUndergradForeign'],
+        total_undergrad_under_25= row['TotalUndergradUnder25'],
+        total_undergrad_over_25= row['TotalUndergradOver25'],
+        total_grad_under_25= row['TotalGradUnder25'],
+        total_grad_over_25= row['TotatlGradOver25']
     )
 end
 """
-
 
 
